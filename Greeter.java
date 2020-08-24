d8ff61615a9997f1ea363ec779d2a512fa76c7c6
Victor Nonato <vnonato@xula.edu>
2020-08-24 17:17:51 +0000

    First comline stuff

--- Greeter.java
+++ Greeter.java
@@ -1,9 +1,9 @@
 public class Greeter{
 	public static void main(String[]args){
 		String name;
-		if (args.lenth>0){
+		if (args.length>0){
 			name=args[0];
-		} else name="Earthling"
+		} else name="Earthling";
 		System.out.println("Hi, "+name);
 	}
 }
